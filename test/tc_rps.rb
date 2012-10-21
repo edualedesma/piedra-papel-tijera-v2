a836143de34c05090f57c4427b69d712dc839fe5
alu3286 <Edinho@MacBook-Pro-Edinho.local>
2012-10-21 12:33:56 +0100

    Modificacion de los test

--- test/tc_rps.rb
+++ test/tc_rps.rb
@@ -10,8 +10,26 @@ class TestRockPaperScissors < Test::Unit::TestCase
 	def teardown
 	end
 	
+	#def test_play
+		#assert_equal()
+	#end
+	
+	def correct_answer(x)
+		return true if x =~ /There is a tie/
+		return true if x =~ /Computer wins; (\w+) defeats (\w+)/ and RockPaperScissors.defeat[$1.to_sym] == $2.to_sym
+		return true if x =~ /Well done. (\w+) beats (\w+)/ and RockPaperScissors.defeat[$1.to_sym] == $2.to_sym
+	end
+
 	def test_play
-		
+		30.times do
+			assert correct_answer(RockPaperScissors.play('scissors'))
+		end
+		30.times do
+			assert correct_answer(RockPaperScissors.play('paper'))
+		end
+		30.times do
+			assert correct_answer(RockPaperScissors.play('rock'))
+		end
 	end
 	
 	# Cuando se juega mal
@@ -19,13 +37,12 @@ class TestRockPaperScissors < Test::Unit::TestCase
 		assert_raises(SyntaxError) {RockPaperScissors.play('tutu')}
 	end
 	
-	def test_variety
-		answers = []
-		
+	#def test_variety
+	#	answers = []
 		# 20 jugadas.
-		20.times do
+	#	20.times do
 			#answer.push RockPaperScissors.throw # no está este método.
-		end
-	end
+	#	end
+	#end
 
 end
